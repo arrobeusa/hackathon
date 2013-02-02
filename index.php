d393d567ae44752b37d48ad7080a70ac2ffb7839
Rob <robert@rotarius.com>
2013-02-02 16:02:40 -0500

    Added jQuery UI

--- index.php
+++ index.php
@@ -57,6 +57,7 @@
     </div> <!-- /container -->
 
     <script src="/assets/js/jquery.js"></script>
+    <script src="/assets/js/jquery-ui/js/jquery-ui.min.js"></script>
     <script src="/assets/js/knockout.js"></script>
     <script src="/assets/js/bootstrap.min.js"></script>
     <script src="/assets/js/main.js"></script>
