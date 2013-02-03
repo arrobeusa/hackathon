78f87835834521e98c8b2eba0e192e1d1a1bc6de
Rob <robert@rotarius.com>
2013-02-03 02:13:50 -0500

    A little refactoring

--- includes/_bottom.php
+++ includes/_bottom.php
@@ -1,4 +1,14 @@
-
-
+    <div class="well">
+      <div class="span4">
+        <ul class="inline">
+          <li><a href="#">about</a></li>
+          <li><a href="#">about</a></li>
+          <li><a href="#">about</a></li>
+        </ul> 
+      </div>
+      <div class="span4" style="float: right;">
+      &copy;Samplefy 2013
+      </div>
+    </div>
   </body>
 </html>
