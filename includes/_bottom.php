cb4d1e40a800aa1207eea1fe648d194c9a42c54e
Michael Johnson <mike@michaelpjohnson.com>
2013-02-08 10:34:22 -0500

    Updates for live website

--- includes/_bottom.php
+++ includes/_bottom.php
@@ -1,9 +1,8 @@
     <div class="well">
       <div class="span4">
         <ul class="inline">
-          <li><a href="#">about</a></li>
-          <li><a href="#">about</a></li>
-          <li><a href="#">about</a></li>
+          <li><a href="http://home2.kickoffpages.com/">about</a></li>
+          <li><a href="mailto:info@samplefy.com">contact us</a></li>
         </ul> 
       </div>
       <div class="span4" style="float: right;">
